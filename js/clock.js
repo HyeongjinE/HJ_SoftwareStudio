7fa4a25337a4bd3c61d489a494015d40ea75777a
HyeongjinE <gudwls9876@gmail.com>
2023-03-01 21:55:07 +0900

    update0301

---
+++ js/clock.js
@@ -0,0 +1,23 @@
+const clock = document.querySelector("span#clock");
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+    /*
+    const date2 = new Date("2023-12-25 GMT+0900");
+
+    const dDay = date2 - date;
+    const day = Math.floor(dDay/(1000*60*60*24));
+    const hour = Math.floor((dDay/(1000*60*60)) % 24);
+    const minute = Math.floor((dDay/(1000*60)) % 60);
+    const second = Math.floor((dDay/1000) % 60);
+    clock2.innerText = `${day}d ${hour}h ${minute}m ${second}s`;
+    */
+}
+
+getClock();
+setInterval(getClock, 1000);
