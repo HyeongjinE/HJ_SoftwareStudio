87ee67134a81b96294dff1ca51693bc83f5a938d
HyeongjinE <gudwls9876@gmail.com>
2023-06-17 14:05:53 +0900

    update

--- js/java.js
+++ js/java.js
@@ -39,20 +39,20 @@ function developClick() {
     alert("준비 중입니다!")
 }
 
-develop.addEventListener("click", developClick)
-explainTrigger.addEventListener("mouseover", function() {
-    explain.classList.remove("hidden");
-    develop.style.top = "-30px";
-})
-explainTrigger.addEventListener("mouseout", function() {
-    explain.classList.add("hidden");
-    develop.style.top = "0px";
-})
-explain.addEventListener("mouseover", function() {
-    explain.classList.remove("hidden");
-    develop.style.top = "-30px";
-})
-explain.addEventListener("mouseout", function() {
-    explain.classList.add("hidden");
-    develop.style.top = "0px";
-})
+// develop.addEventListener("click", developClick)
+// explainTrigger.addEventListener("mouseover", function() {
+//     explain.classList.remove("hidden");
+//     develop.style.top = "-30px";
+// })
+// explainTrigger.addEventListener("mouseout", function() {
+//     explain.classList.add("hidden");
+//     develop.style.top = "0px";
+// })
+// explain.addEventListener("mouseover", function() {
+//     explain.classList.remove("hidden");
+//     develop.style.top = "-30px";
+// })
+// explain.addEventListener("mouseout", function() {
+//     explain.classList.add("hidden");
+//     develop.style.top = "0px";
+// })
